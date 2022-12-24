d89991639c7da760c9ff5d1c3af8a5f31479ce5b
PiThon <87096212+ProOfPython@users.noreply.github.com>
2022-12-23 17:19:36 -0800

    Add files via upload

---
+++ ADV 117-119 Class/Sketch_Identification/main.js
@@ -0,0 +1,32 @@
+function clearCanvas(){
+    background('white')
+}
+function classifyCanvas(){
+    classifier.classify(canvas, gotRusult)
+}
+function gotResult(error, results){
+    if (error){
+        console.error(error)
+    } else {
+        console.log(results)
+        document.getElementById('drawing').innerHTML = 'Drawing: ' + results[0].label
+        document.getElementById('confidence').innerHTML = 'Confidence: ~' + Math.round(results[0].confidence * 100) + '%'
+    }
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet')
+}
+function setup(){
+    canvas = createCanvas(280, 280)
+    canvas.center()
+    background('white')
+    canvas.mouseRealeased(classifyCanvas)
+}
+function draw(){
+    strokeWeight(12.5)
+    stroke(0)
+    if (mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY)
+    }
+}
